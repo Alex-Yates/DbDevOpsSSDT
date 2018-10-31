283677431c76c1fc41683bf19317b5b5ff7a5f51
Alex Yates <Alex-Yates@users.noreply.github.com>
2018-10-31 11:11:26 +0000

    Improving accuracy of comment

--- AdventureWorksDB/DeployStaticData.sql
+++ AdventureWorksDB/DeployStaticData.sql
@@ -1,4 +1,4 @@
--- Updates all static data tables by executing stored procedures
+-- Updates static data tables by executing corresponding stored procedures
 
 PRINT 'Updating Sales.Currency data'
 EXEC data.uspSales_Currency
