c83f8fca2b47d83882050a755ae0ba96749e8573
Alex Yates <Alex-Yates@users.noreply.github.com>
2018-11-02 00:55:23 +0000

    Create PSBuildScript.ps1

---
+++ PSBuildScript.ps1
@@ -0,0 +1,13 @@
+# PowerShell to deploy a Dacpac from Azure DevOps - requires sqlpackage.exe to be installed and added to PATH
+
+$args = @("-SourceFile:$env:dacpac",
+    "-Action:Publish",
+    "-Profile:$env:profile"
+)
+
+Write-Host "Executing: & sqlpackage $args"
+& sqlpackage $args
+
+# Requires the following env variables to be set
+# dacpac | $(System.DefaultWorkingDirectory)/_SqlServerCentral20181101-.NET Desktop-CI/drop/SqlServerCentralDB/SqlServerCentral/bin/Release/SqlServerCentral.dacpac
+# profile | $(System.DefaultWorkingDirectory)/_SqlServerCentral20181101-.NET Desktop-CI/drop/SqlServerCentralDB/SqlServerCentral/bin/Release/int.publish.xml
